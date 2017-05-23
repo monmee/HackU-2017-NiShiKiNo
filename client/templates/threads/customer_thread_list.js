1fd1938ef6eea73b168be8a1b8785ef0cf425886
Nymphaea808 <gingersyrup808@gmail.com>
2017-05-23 18:00:00 +0900

    mod customer thread list

--- client/templates/threads/customer_thread_list.js
+++ client/templates/threads/customer_thread_list.js
@@ -1,10 +1,14 @@
 var threadData=[
   {
     title:'カレーのアレ',
-    date:'2017/05/23'},
+    date:'2017/05/23',
+    status: 'processing'
+  },
   {
     title:'シンセのACアダプター',
-    date: '2017/05/26'}
+    date: '2017/05/26',
+    status:'done'
+  }
   ];
 
 Template.customerThreadList.helpers({
