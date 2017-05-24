c536653b5752820cbac80fae287e12cc798552bf
monmee <jazzmonmee@gmail.com>
2017-05-24 20:12:21 +0900

    disable autopublish & collection rename

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,9 @@
 // import { Threads } from '../lib/collections/collections.js'
 
 Meteor.publish('threads',function(){
-  // return Threads.find();
+  return Threads.find();
+})
+
+Meteor.publish('passengers', function(){
+  return Passengers.find();
 })
