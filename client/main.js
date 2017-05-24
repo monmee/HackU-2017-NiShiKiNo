9361081bbde1ca5f905acf4b83d836ba041fa707
monmee <jazzmonmee@gmail.com>
2017-05-24 20:12:21 +0900

    disable autopublish & collection rename

--- client/main.js
+++ client/main.js
@@ -2,4 +2,5 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import '../imports/startup/accounts-config.js';
 
-// Meteor.subscribe('threads');
+Meteor.subscribe('threads');
+Meteor.subscribe('passengers');
