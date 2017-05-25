ee8e786973a207cddf5e17522535f5e768893564
monmee <jazzmonmee@gmail.com>
2017-05-25 16:04:58 +0900

    色々change

---
+++ client/templates/edits/edit_profile.js
@@ -0,0 +1,25 @@
+import { Meteor } from 'meteor/meteor';
+import './edit_profile.html';
+import SimpleSchema from 'simpl-schema';
+SimpleSchema.extendOptions(['autoform']);
+
+
+Template.editProfile.helpers({
+  username: function() 
+  {
+    return Meteor.user().username;
+  }
+});
+Template.editProfile.events({
+  '.submit .update-profile': function (e) {
+      e.preventDefault();
+      Posts.insert({
+        message: 'hello!'
+        // username: Meteor.user().username,
+        // password: document.getElementsByName('new-pass'),
+        // profile_image: document.getElementsByName('media[]'),
+        // profile_comment: document.getElementsByName('comment')
+      });
+      Router.route('/cthread',{name:'customerThreadList'});
+    }
+});
