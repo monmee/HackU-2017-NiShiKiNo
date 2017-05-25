a6336b2bf1b7aeb78dda7c2935c9d9df41ecc7de
monmee <jazzmonmee@gmail.com>
2017-05-25 11:13:55 +0900

    import edit

--- client/edits/edit_profile.js
+++ client/edits/edit_profile.js
@@ -1,8 +1,12 @@
+import { Meteor } from 'meteor/meteor';
+import './edit_profile.html';
+
 Template.editProfile.events({
   '.submit .updateProfile': function (e) {
       e.preventDefault();
       Passsengers.insert({
-        username: document.getElementsByName('user-name'),
+        user_id: Meteor.userId(),
+        username: Meteor.user().username,
         password: document.getElementsByName('new-pass'),
         profile_image: document.getElementsByName('media[]'),
         profile_comment: document.getElementsByName('comment')
