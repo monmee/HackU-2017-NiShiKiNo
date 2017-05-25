90f9094c29a1bf2e4f380f32c3600d1cf55cb3f0
idealexe <ideal.exe@live.jp>
2017-05-25 10:47:58 +0000

    don't show location alert

--- client/main.js
+++ client/main.js
@@ -1,6 +1,5 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
-import '../imports/startup/accounts-config.js';
 
 Meteor.subscribe('threads');
 Meteor.subscribe('passengers');
