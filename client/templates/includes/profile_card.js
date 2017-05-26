55d627d5673d89da78fd4a1e15d1517fea32626b
ideal.exe <ideal.exe@live.jp>
2017-05-26 16:33:46 +0900

    fix

--- client/templates/includes/profile_card.js
+++ client/templates/includes/profile_card.js
@@ -1,6 +1,6 @@
 Template.profileCard.helpers({
     name: "Kiri Man",
-    email: Meteor.user().emails[0].address,
+    email: Meteor.user().emails.[0].address,
     age: 24,
     address: "Toky, Japan",
     comment: "Hi! I am Kiri man.",
