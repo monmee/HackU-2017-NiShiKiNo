752718f944d07a96e566682fb02619c8971451c6
Nymphaea808 <gingersyrup808@gmail.com>
2017-05-26 19:59:53 +0900

    johooooooooooooooooooooogit add .

--- client/main.js
+++ client/main.js
@@ -3,5 +3,5 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import { check } from 'meteor/check';
 
 // Meteor.subscribe('threads');
-
+Meteor.subscribe('posts');
 Meteor.subscribe('records');
